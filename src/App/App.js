92ce5e1a7e1b6163ed7552ec4b5fd17d4c0287b9
Brooke Nemchak <bnemchak@gmail.com>
2020-09-01 22:31:59 -0400

    setup wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import Auth from '../components/pages/Auth/Auth';
+import EditItem from '../components/pages/EditItem/EditItem';
+import Home from '../components/pages/Home/Home';
+import MyNavbar from '../components/pages/MyNavbar/MyNavbar';
+import NewItem from '../components/pages/NewItem/NewItem';
+import SingleItem from '../components/pages/SingleItem/SingleItem';
+
+import fbConnection from '../helpers/data/connection';
+
 import './App.scss';
 
 class App extends React.Component {
